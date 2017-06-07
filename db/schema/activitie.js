a0aa4933b1e2365fd89e1c6a099e2c7401eab628
liyan <609505217@qq.com>
2017-06-07 22:22:40 +0800

    change collection

--- db/schema/activitie.js
+++ db/schema/activitie.js
@@ -1,15 +1,21 @@
 var mongoose = require('../db.js')
 
 var ActivitieSchema = new mongoose.Schema({
-	bus_id:Number,
-	bus_avatar:String,
-	bus_number:Number,
-	bus_introduce:{
-	    bus_name:String,
-	    bus_location:String,
-	    bus_info:String,
-	    bus_phone:String
-	}
+    bus_id:Number,
+    act_id:Number,
+    act_name:String, //用户自己设置，有默认值
+    act_avatar:String,
+    act_date:Date, //活动时间
+    member:{
+        max:Number,
+        min:Number,
+        now:Number
+    },
+    act_originator:{ //活动发起人信息
+        ori_id:Number,
+        ori_phone:String
+    },
+    act_member:[Number] //用户的openid    存用户?用户id
 })
 
 module.exports = mongoose.model('activitie',ActivitieSchema);
